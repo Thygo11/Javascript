4f29c1c2006c0ea33fe558d4683ea1210b6ec89a
Thygo11 <9025896@student.zadkine.nl>
2024-10-23 16:01:28 +0200

    Opdracht 7

---
+++ Opdracht 7/app.js
@@ -0,0 +1,13 @@
+
+let output = document.getElementById('output')
+
+let name = "Thygo"
+let age = 16
+
+output.innerText = name
+output.innerText = age
+
+function showName (){
+output.innerText = name
+
+}
---
+++ Opdracht 7/if.js
@@ -0,0 +1,11 @@
+let hour = 15;
+let product = 60;
+let budget = 100;
+let output = documet.getElementById('output')
+
+
+if (budget > product){   
+output.innertext("ik heb genoeg geld")
+}else {
+output.innertext('ik heb te weinig geld');
+}
