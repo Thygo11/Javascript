dfb35c1127af0bc94866ae67fbebc536e4836ab5
Thygo11 <9025896@student.zadkine.nl>
2024-11-06 15:35:00 +0100

    Opdracht 11

--- Opdracht 10/apps.js
+++ Opdracht 10/apps.js
@@ -1,28 +1,46 @@
 function add() {
-  let num1 = Number(document.getElementById("num1").value);
-let num2 = Number(document.getElementById("num2").value);
- let result = num1 + num2;
- document.getElementById("result").innerText = "Antwoord: " + result
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    if (num1 > 0 && num2 > 0) {
+        let result = num1 + num2;
+        document.getElementById("result").innerText = "Antwoord: " + result
+    } else {
+        document.getElementById("result").innerText = "het getal moet hoger zijn dan 0"
+    }
 }
+
+
 function minus() {
-let num1 = Number(document.getElementById("num1").value);
-let num2 = Number(document.getElementById("num2").value);
-let result = num1 - num2;
- document.getElementById("result").innerText = "Antwoord: " + result
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    if (num1 > 0 && num2 > 0) {
+        let result = num1 + num2;
+        document.getElementById("result").innerText = "Antwoord: " - result
+    } else {
+        document.getElementById("result").innerText = "het getal moet hoger zijn dan 0"
+    }
 }
+
 function multiply() {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    let result = num1 * num2;
-     document.getElementById("result").innerText = "Antwoord: " + result
+    if (num1 > 0 && num2 > 0) {
+        let result = num1 + num2;
+        document.getElementById("result").innerText = "Antwoord: " * result
+    } else {
+        document.getElementById("result").innerText = "het getal moet hoger zijn dan 0"
+    }
 }
+
 function divide() {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
-    if (num2 === 0) {
-        document.getElementById("result").innerText = "Antwoord: Kan niet delen door 0";
+    if (num1 > 0 && num2 > 0) {
+        let result = num1 + num2;
+        document.getElementById("result").innerText = "Antwoord: " / result
     } else {
-        let result = num1 / num2;
-        document.getElementById("result").innerText = "Antwoord: " + result;
+        document.getElementById("result").innerText = "het getal moet hoger zijn dan 0"
     }
 }
+
